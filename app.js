1026b42d24ebefabd7fe3938e74bd151b5a630d4
jae <jl_135@outlook.com>
2024-12-20 19:12:39 +0000

    draft

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("This works")
+
+//First store global variables 
+let cookieCount= 0;
+let cookiesPerSecond= 0;
+//you can start with no cookies in your total each second or 1 cookie
+
+let gameData={
+    cookieCount:0,
+    cookiesPerSecond:0,
+};
+//these values can also be stored in a neat and tidy object containing all the game data
+
+//add upgrades to the array, do it with upgrads from our API
+const shopUgradesArray = [
+    {upgradeOne:"Muffins"},
+    {upgradeTwo:"Donoughts"},
+    {upgradeThree:"Cheesecake"},
+    {upgradeFour:"Cherry Pie"},
+    {upgradeFive:"Super Mega Chip"}
+
+];
+

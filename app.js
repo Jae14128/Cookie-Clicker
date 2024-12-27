e9918fae859aeb3b8979cd209fd05378e125fda1
jae <jl_135@outlook.com>
2024-12-27 21:53:48 +0000

    continual

--- app.js
+++ app.js
@@ -29,4 +29,64 @@ const buttonreset= document.getElementById("reset");
 const divupgradeshop= document.getElementById("upgradeshop");
 
 //retrieve and display upgrades from the API
+const upgradesContainer= document.getElementById("upgrade-shop");
+async function getShopUgrades () {
+    const result = await fetch (
+"https://cookie-upgrade-api.vercel.app/api/upgrades"
+);
 
+//translate the work that'll be received from JSON
+const data=await response.json();
+return [data];
+
+////push to upgrades
+
+};
+
+
+async function provideShopUpgrades() {
+    const getShopItems= await getShopUpgrades();
+    getShopItems.forEach(function (upgrade) {
+        const upgradeName= document.getElementById("upgrade-shop");
+        const upgradeCost= document.getElementById("cookie-gains");
+        const upgradeCPSIncrease= document.getElementById("increase");
+
+        upgradeName.setAttribute("upgrade-shop", "Get an upgrade");
+        upgradeCost.setAttribute("cookie-gains", "Gain an extra cookie");
+        upgradeCPSIncrease.setAttribute("increase", "Increase your score");
+
+        shopUgradesArray.appendChild(upgradeName);
+        shopUgradesArray.appendChild(upgradeCost);
+        shopUgradesArray.appendChild(upgradeCPSIncrease);
+
+        cookies-owned.addEventListener('click', handleClick);
+        cookies-gains.addEventListener('click', handleClick);
+        shop-upgrade.addEventListener('click', handleClick);
+
+        function handleButtonClick() {
+         const buttonincrease=document.getElementById("increase");
+            buttonincrease.addEventListener("click", handleButtonClick);
+        }
+    })};
+
+    renderShopUpgrades();
+
+const pcookiesowned= document.getElementById("cookies-owned");
+pcookies.addEventListener("click", handleClick);
+
+
+const pcookiegains= document.getElementById("cookie-gains");
+pcookiegains.addEventListener("click", handleClick);
+
+
+const buttonreset= document.getElementById("reset");
+buttonreset.addEventListener("click", handleClick);
+
+
+const divupgradeshop= document.getElementById("upgradeshop");
+divupgradeshop.addEventListener("click", handleClick);
+
+
+function handleUpgradeClick() {
+    
+}
